9de4b3513548850d1829208c9aca2a47aa9233d8
Ald28 <aldopichihua123@gmail.com>
2024-08-22 11:32:03 -0500

    nodejs con html

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const routes = require('./rutas.js');
+const port = 3000
+
+app.use(routes)
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+// app.get('/', (req, res) => {
+//     res.sendFile('./static/index.html',{
+//         root: __dirname
+//     });
+// })
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
